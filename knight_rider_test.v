299ada989b49b970951ba6a6683ae42f38d67111
kiiichi <kichi3744@outlook.com>
2023-07-19 21:11:07 +0800

    Create knight_rider_test.v

---
+++ knight_rider_test.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2023/07/19 20:55:09
+// Design Name: 
+// Module Name: knight_rider_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module knight_rider_test(
+    input clk,
+    );
+endmodule
