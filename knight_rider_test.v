26a5af6c695b4e2c3e6398558fb2577858ec41aa
Zhu Xuezhi <kichi3744@outlook.com>
2023-07-20 01:39:14 +0800

    Update knight_rider_test.v

--- knight_rider_test.v
+++ knight_rider_test.v
@@ -29,4 +29,29 @@ module knight_rider_test(
     parameter LEDS_INIT = 10'b1100000000;
     parameter DIR_INIT = 1;
 
+    reg [9:0] leds = LEDS_INIT; // register for led output
+    reg [3:0] position = DIR_INIT*8; // state counter 0->15
+    reg direction = DIR_INIT;   // direction indicator
+
+    always @ (posedge clk) begin
+        if (direction == 0) begin
+            leds <= leds << 1;  // bit-shift leds register
+        end 
+        else begin
+            leds <= leds >> 1;  // bit-shift leds register
+        end
+        position <= position + 1;
+    end
+
+    always @ (position) begin       // change direction
+        if (position < 8) begin     // in the second half
+            direction = 0;
+        end 
+        else begin
+            direction = 1;
+        end
+    end
+
+    assign led_out = leds[8:1]; // wire output and leds register
+    
 endmodule
