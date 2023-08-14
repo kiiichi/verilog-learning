9ec4d56480625686708b986a785d720089127e7b
kiiichi <kichi3744@outlook.com>
2023-08-14 19:43:44 +0800

    Update float32_to_binary.py

--- code/float32_to_binary.py
+++ code/float32_to_binary.py
@@ -1,19 +1,21 @@
 import struct
 
-# def float_to_binary32(value):
-#     # 使用struct将浮点数转换为单精度的字节表示
-#     packed_value = struct.pack('!f', value)
+def float_to_binary32(value):
+    # 使用struct将浮点数转换为单精度的字节表示
+    packed_value = struct.pack('!f', value)
     
-#     # 提取二进制位
-#     binaries = ''.join(f'{byte:08b}' for byte in packed_value)
-#     return binaries
+    # 提取二进制位
+    binaries = ''.join(f'{byte:08b}' for byte in packed_value)
+    return binaries
 
-# # 测试
-# value = 3.14
-# binary_representation = float_to_binary32(value)
-# print(binary_representation)
 
-test = struct.pack('!f', -2.25)
-print(test)
-binary = ''.join((format(b, '08b') for b in test))
-print(binary)
+def float_to_int32(value):
+    packed_value = struct.pack('!f', value)
+    # 将字节转换为整数
+    return struct.unpack('!I', packed_value)[0]
+
+test_bin = float_to_int32(1)
+test_int = float_to_int32(1)
+print(test_bin)
+print(test_int)
+print(bin(test_int))
